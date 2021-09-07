fcf985b0bb66edba13caf8c786f00b7d5dc4434e
Yacine Jernite <yacine.jernite@gmail.com>
2021-09-07 17:47:04 -0500

    added perplexities but still have a caching problem

--- streamlit_data_demo.py
+++ streamlit_data_demo.py
@@ -15,6 +15,25 @@ from datasets import (
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+st.set_page_config(
+    page_title="Demo to showcase dataset metrics",
+    page_icon="https://huggingface.co/front/assets/huggingface_logo.svg",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+# colorblind-friendly colors
+colors = [
+    "#332288",
+    "#117733",
+    "#882255",
+    "#AA4499",
+    "#CC6677",
+    "#44AA99",
+    "#DDCC77",
+    "#88CCEE",
+]
+
 ########## preparation functions
 
 # list of fiels we want to keep around from the DatasetInfo object
@@ -99,14 +118,29 @@ def get_config_infos_dict(name):
         for config_info in get_config_infos(name)
     }
 
+####### prepare text and scores
 # extract text to analyze:
 # list of all examples of a given feature in a given split of a given config of a given dataset
-# returns a list of strings
+# returns a dataset of strings
+def extract_text(examples, text_path):
+    text_list = []
+    example_ct = 0
+    item_list = examples[text_path[0]]
+    for field_name in text_path[1:]:
+        item_list = [
+            next_item
+            for item in item_list
+            for next_item in (item[field_name] if isinstance(item[field_name], list) else [item[field_name]])
+        ]
+    text_list += [
+        text
+        for item in item_list
+        for text in (item if isinstance(item, list) else [item])
+    ]
+    return {"text": text_list}
+
 @st.cache(persist=True, allow_output_mutation=True)
-def get_text_to_analyze(
-    name, text_path, config,
-    split=None, max_items=20000, streaming=False
-):
+def get_text_to_analyze(name, text_path, config, split=None, max_items=20000, streaming=False):
     ### default arguments
     if split is None:
         split = 'train' if 'train' in config["splits"] else list(config["splits"])[0]
@@ -114,70 +148,24 @@ def get_text_to_analyze(
     ### get text from dataset
     print(f"running -- load_dataset({name}, {config['config_name']}, streaming={streaming})")
     dataset = load_dataset(name, config["config_name"], streaming=streaming)
-    text_list = []
-    example_ct = 0
-    for example in dataset[split]:
-        example_ct += 1
-        # robustly handle fields that contain lists of text
-        item_list = [example]
-        for field_name in text_path:
-            item_list = [
-                next_item
-                for item in item_list
-                for next_item in (item[field_name] if isinstance(item[field_name], list) else [item[field_name]])
-            ]
-        text_list += [
-            text
-            for item in item_list
-            for text in (item if isinstance(item, list) else [item])
-        ]
-        if example_ct >= max_items:
-            break
-    return text_list
-
+    data_split = dataset[split].select(range(max_items))
+    dataset_text = data_split.map(
+        lambda examples: extract_text(examples, text_path),
+        batched=True,
+        remove_columns=data_split.column_names,
+    )
+    return dataset_text
 
 ########## metrics code
-@st.cache(persist=True, allow_output_mutation=True)
-def get_space_tokenized_lengths(sentences):
-    lengths = [len(st.split()) for st in sentences]
-    return lengths
-
-
-# TODO: cache results
 tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
 model = AutoModelForCausalLM.from_pretrained("xlnet-base-cased")
-# @st.cache(persist=True, allow_output_mutation=True, hash_funcs={tokenizers.Tokenizer: (lambda _:None), tokenizers.AddedToken: (lambda _:None)})
 def get_single_sent_loss(sent):
     batch = tokenizer(sent, return_tensors="pt", padding=True)
     batch['labels'] = batch['input_ids']
     loss = model(**batch).loss.item()
     return loss
 
-@st.cache(persist=True, allow_output_mutation=True, hash_funcs={tokenizers.Tokenizer: (lambda _:None), tokenizers.AddedToken: (lambda _:None)})
-def get_gpt2_perplexities(sentences):
-    losses = [get_single_sent_loss(st) for st in sentences]
-    return losses
-
 ########## streamlit code
-
-st.set_page_config(
-    page_title="Demo to showcase dataset metrics",
-    page_icon="https://huggingface.co/front/assets/huggingface_logo.svg",
-    layout="wide",
-    initial_sidebar_state="auto",
-)
-
-# colorblind-friendly colors
-colors = [
-    "#332288",
-    "#117733",
-    "#882255",
-    "#AA4499",
-    "#CC6677",
-    "#44AA99",
-    "#DDCC77",
-    "#88CCEE",
-]
 
 ds_list = list_datasets(with_community_datasets=False, with_details=True)
 ds_name_to_dict = {ds.id: ds.description for ds in ds_list}
@@ -280,67 +268,34 @@ with st.sidebar.expander("Choose second dataset and field"):
 
 
 # Grab the text requested
-text_to_analyze_a = get_text_to_analyze(
+text_dset_a = get_text_to_analyze(
     ds_name_a, text_feature_a, ds_config_a,
     split=split_a, max_items=num_examples_a, streaming=streaming_a
 )
-
-text_to_analyze_b = get_text_to_analyze(
+text_dset_b = get_text_to_analyze(
     ds_name_b, text_feature_b, ds_config_b,
     split=split_b, max_items=num_examples_b, streaming=streaming_b
 )
+
+text_dset_a_lengths = text_dset_a.map(lambda exple: {"space_tok_length": len(exple["text"].split())})
+text_dset_b_lengths = text_dset_b.map(lambda exple: {"space_tok_length": len(exple["text"].split())})
+
+text_dset_a_loss = text_dset_a.map(lambda exple: {"xlnet_loss": get_single_sent_loss(exple["text"])})
+text_dset_b_loss = text_dset_b.map(lambda exple: {"xlnet_loss": get_single_sent_loss(exple["text"])})
 
 ######## Main window
 
 left_col, right_col = st.columns(2)
 
+### First, show some example texts from the dataset
 left_col.markdown(f"### Showing {ds_name_a} - {config_name_a} - {text_feature_a}")
 with left_col.expander("Dataset Description A"):
     st.markdown(ds_name_to_dict[ds_name_a])
 
 with left_col.expander("Show some examples A"):
     st.markdown("### Example text fields A")
-    start_id_show = st.slider('Starting index A:', 0, len(text_to_analyze_a) - 5, value=0, step=5)
-    st.dataframe(text_to_analyze_a[start_id_show:start_id_show+10])
-
-with left_col.expander("Show text lengths A", expanded=True):
-    st.markdown("### Text lengths A")
-    space_tok_lengths_a = get_space_tokenized_lengths(text_to_analyze_a)
-    hist_data_a = [space_tok_lengths_a]
-    fig_a = ff.create_distplot(hist_data_a, group_labels=["text lengths"])
-    st.plotly_chart(fig_a, use_container_width=True)
-    sorted_sents_lengths_a = [
-        s for s, l in sorted(
-            [(s, l) for s, l in zip(text_to_analyze_a, space_tok_lengths_a)],
-            key=lambda x:x[1], reverse=True,
-        )
-    ]
-    start_id_show_lengths_a = st.slider(
-        'Show longest sentences in A starting at index:',
-        0, len(text_to_analyze_a) - 5, value=0, step=5
-    )
-    for sent in sorted_sents_lengths_a[start_id_show_lengths_a:start_id_show_lengths_a+5]:
-        st.text(sent)
-
-with left_col.expander("Show text perplexities A", expanded=True):
-    st.markdown("### Text perplexities A")
-    perplexities_a = get_gpt2_perplexities(text_to_analyze_a)
-    hist_data_loss_a = [perplexities_a]
-    fig_loss_a = ff.create_distplot(hist_data_loss_a, group_labels=["text perplexities"])
-    st.plotly_chart(fig_loss_a, use_container_width=True)
-    sorted_sents_loss_a = [
-        s for s, l in sorted(
-            [(s, l) for s, l in zip(text_to_analyze_a, perplexities_a)],
-            key=lambda x:x[1], reverse=True,
-        )
-    ]
-    start_id_show_loss_a = st.slider(
-        'Show highest perplexity sentences in A starting at index:',
-        0, len(text_to_analyze_a) - 5, value=0, step=5
-    )
-    for sent in sorted_sents_loss_a[start_id_show_loss_a:start_id_show_loss_a+5]:
-        st.text(sent)
-
+    start_id_show = st.slider('Starting index A:', 0, text_dset_a.num_rows - 5, value=0, step=5)
+    st.dataframe(text_dset_a[start_id_show:start_id_show+10]["text"])
 
 right_col.markdown(f"### Showing {ds_name_b} - {config_name_b} - {text_feature_b}")
 with right_col.expander("Dataset Description B"):
@@ -348,11 +303,79 @@ with right_col.expander("Dataset Description B"):
 
 with right_col.expander("Show some examples B"):
     st.markdown("### Example text fields B")
-    start_id_show = st.slider('Starting index B:', 0, len(text_to_analyze_b) - 10, 5)
-    st.dataframe(text_to_analyze_b[start_id_show:start_id_show+10])
+    start_id_show = st.slider('Starting index B:', 0, text_dset_b.num_rows - 10, 5)
+    st.dataframe(text_dset_b[start_id_show:start_id_show+10]["text"])
+
+### Second, show the distribution of text lengths
+with left_col.expander("Show text lengths A", expanded=True):
+    st.markdown("### Text lengths A")
+    hist_data_tok_length_a = [text_dset_a_lengths["space_tok_length"]]
+    fig_tok_length_a = ff.create_distplot(hist_data_tok_length_a, group_labels=["text lengths"])
+    st.plotly_chart(fig_tok_length_a, use_container_width=True)
+    sorted_sents_lengths_a = [
+        s for s, l in sorted(
+            [(sentence["text"], sentence["space_tok_length"]) for sentence in text_dset_a_lengths],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    start_id_show_lengths_a = st.slider(
+        'Show longest sentences in A starting at index:',
+        0, text_dset_a.num_rows - 5, value=0, step=5
+    )
+    for sent in sorted_sents_lengths_a[start_id_show_lengths_a:start_id_show_lengths_a+5]:
+        st.text(sent)
 
 with right_col.expander("Show text lengths B", expanded=True):
     st.markdown("### Text lengths B")
-    hist_data_b = [[len(st.split()) for st in text_to_analyze_b]]
-    fig_b = ff.create_distplot(hist_data_b, group_labels=["text lengths"])
-    st.plotly_chart(fig_b, use_container_width=True)
+    hist_data_tok_length_b = [text_dset_b_lengths["space_tok_length"]]
+    fig_tok_length_b = ff.create_distplot(hist_data_tok_length_b, group_labels=["text lengths"])
+    st.plotly_chart(fig_tok_length_b, use_container_width=True)
+    sorted_sents_lengths_b = [
+        s for s, l in sorted(
+            [(sentence["text"], sentence["space_tok_length"]) for sentence in text_dset_b_lengths],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    start_id_show_lengths_b = st.slider(
+        'Show longest sentences in B starting at index:',
+        0, text_dset_a.num_rows - 5, value=0, step=5
+    )
+    for sent in sorted_sents_lengths_b[start_id_show_lengths_b:start_id_show_lengths_b+5]:
+        st.text(sent)
+
+### Third, show the distribution of text perplexities
+with left_col.expander("Show text perplexities A", expanded=True):
+    st.markdown("### Text perplexities A")
+    hist_data_loss_a = [text_dset_a_loss["xlnet_loss"]]
+    fig_loss_a = ff.create_distplot(hist_data_loss_a, group_labels=["text perplexity"])
+    st.plotly_chart(fig_loss_a, use_container_width=True)
+    sorted_sents_loss_a = [
+        s for s, l in sorted(
+            [(sentence["text"], sentence["xlnet_loss"]) for sentence in text_dset_a_loss],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    start_id_show_loss_a = st.slider(
+        'Show highest perplexity sentences in A starting at index:',
+        0, text_dset_a.num_rows - 5, value=0, step=5
+    )
+    for sent in sorted_sents_loss_a[start_id_show_loss_a:start_id_show_loss_a+5]:
+        st.text(sent)
+
+with right_col.expander("Show text perplexities B", expanded=True):
+    st.markdown("### Text perplexities B")
+    hist_data_loss_b = [text_dset_b_loss["xlnet_loss"]]
+    fig_loss_b = ff.create_distplot(hist_data_loss_b, group_labels=["text perplexity"])
+    st.plotly_chart(fig_loss_b, use_container_width=True)
+    sorted_sents_loss_b = [
+        s for s, l in sorted(
+            [(sentence["text"], sentence["xlnet_loss"]) for sentence in text_dset_b_loss],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    start_id_show_loss_b = st.slider(
+        'Show highest perplexity sentences in B starting at index:',
+        0, text_dset_b.num_rows - 5, value=0, step=5
+    )
+    for sent in sorted_sents_loss_b[start_id_show_loss_b:start_id_show_loss_b+5]:
+        st.text(sent)
