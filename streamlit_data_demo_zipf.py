9622de62a248bdef5e3e263e93d293a7ae42dfc2
Sasha <luccionis@mila.quebec>
2021-09-15 16:48:44 -0400

    Fixing coefficient in Zipf

--- streamlit_data_demo_zipf.py
+++ streamlit_data_demo_zipf.py
@@ -409,7 +409,7 @@ with left_col.expander("Show the Zipf distribution for A", expanded=True):
     fit_a = powerlaw.Fit(term_freq_df_a.total, discrete= True)
     s_a = fit_a.power_law.alpha
     sig_a = fit_a.power_law.sigma
-    expected_zipf_a = [int(term_freq_df_a['total'][0]/(i+1)**np.log(s_a)) for i in np.arange(_SAMPLE_SIZE)]
+    expected_zipf_a = [int(term_freq_df_a['total'][0]/(i+1)**s_a) for i in np.arange(_SAMPLE_SIZE)]
     fig_zipf_a = go.Figure()
     fig_zipf_a.add_trace(go.Scatter(x=np.arange(_SAMPLE_SIZE), y=expected_zipf_a, name = 'Expected', line=dict(color='#332288')))
     fig_zipf_a.add_trace(go.Bar(x=np.arange(_SAMPLE_SIZE), y= term_freq_df_a['total'], hovertext=term_freq_df_a.index.tolist(), name='Observed', marker_color= "#882255", opacity=0.99))
@@ -437,7 +437,7 @@ with right_col.expander("Show the Zipf distribution for B", expanded=True):
     fit_b = powerlaw.Fit(term_freq_df_b.total, discrete= True)
     s_b = fit_b.power_law.alpha
     sig_b = fit_b.power_law.sigma
-    expected_zipf_b = [int(term_freq_df_b['total'][0]/(i+1)**np.log(s_b)) for i in np.arange(_SAMPLE_SIZE)]
+    expected_zipf_b = [int(term_freq_df_b['total'][0]/(i+1)**s_b) for i in np.arange(_SAMPLE_SIZE)]
     fig_zipf_b = go.Figure()
     fig_zipf_b.add_trace(go.Scatter(x=np.arange(_SAMPLE_SIZE), y=expected_zipf_b, name = 'Expected', line=dict(color='#332288')))
     fig_zipf_b.add_trace(go.Bar(x=np.arange(_SAMPLE_SIZE), y= term_freq_df_b['total'], hovertext=term_freq_df_b.index.tolist(), name='Observed', marker_color= "#882255", opacity=0.99))
