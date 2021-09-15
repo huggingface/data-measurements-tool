590786ce87c21875619e52fd6507e6c3aede9fd8
Yacine Jernite <yacine.jernite@gmail.com>
2021-09-15 10:14:25 -0500

    caching fixed, removed useless stuff

--- streamlit_data_demo.py
+++ streamlit_data_demo.py
@@ -3,17 +3,18 @@ import numpy as np
 import plotly.figure_factory as ff
 import streamlit as st
 import tokenizers
+import transformers
 import torch
 
+from dataclasses import asdict
 from datasets import (
-    import_main_class,
+    Dataset,
+    get_dataset_infos,
     list_datasets,
     load_dataset,
     load_dataset_builder,
-    prepare_module,
 )
 from os.path import join as pjoin
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
 st.set_page_config(
     page_title="Demo to showcase dataset metrics",
@@ -36,6 +37,16 @@ colors = [
 
 ########## preparation functions
 
+_SAMPLE_SIZE = 5000
+
+@st.cache()
+def all_datasets():
+    ds_list = list_datasets(with_community_datasets=False, with_details=True)
+    ds_name_to_dict = {ds.id: ds.description for ds in ds_list}
+    ds_names = list(ds_name_to_dict.keys())
+    return (ds_names, ds_name_to_dict)
+
+
 # list of fiels we want to keep around from the DatasetInfo object
 keep_info_fields = [
     "features",
@@ -43,43 +54,27 @@ keep_info_fields = [
     "splits",
 ]
 
-# get DatasetInfo object without downloading the dataset
-def get_config_infos(name):
-    module_path, *_ = prepare_module(name, dataset=True)
-    builder_cls = import_main_class(module_path, dataset=True)
-    configs = [c.name for c in builder_cls.BUILDER_CONFIGS] or [None]
-    if len(configs) == 1:
-        info_dict = load_dataset_builder(name).info.__dict__
-        return [
-            {k: info_dict[k] for k in keep_info_fields}
-        ]
-    else:
-        config_list = []
-        for config_name in configs:
-            info_dict = load_dataset_builder(name, config_name).info.__dict__
-            config_list += [
-                {k: info_dict[k] for k in keep_info_fields}
-            ]
-        return config_list
-
 # Recursively get a list of all features of a certain dtype
 # the output is a list of tuples > e.g. ('A', 'B', 'C') for feature example['A']['B']['C']
+@st.cache(allow_output_mutation=True)
 def get_typed_features(features, ftype='string', parents=None):
     if parents is None:
         parents = []
     typed_features = []
     for name, feat in features.items():
-        if hasattr(feat, 'dtype') and feat.dtype == ftype:
+        if feat.get("dtype", None) == ftype:
             typed_features += [tuple(parents + [name])]
-        elif hasattr(feat, 'feature'):
-            if hasattr(feat.feature, 'dtype') and feat.feature.dtype == ftype:
+        elif "feature" in feat:
+            if feat["feature"].get("dtype", None) == ftype:
                 typed_features += [tuple(parents + [name])]
-            elif isinstance(feat.feature, dict):
+            elif isinstance(feat["feature"], dict):
                 typed_features += get_typed_features(feat.feature, ftype, parents + [name])
     return typed_features
 
 # Recursively get a list of all features that are ClassLabels
 # the outputs are pairs of tuples as above and the list of class names
+# TODO: fix for DICT
+@st.cache(allow_output_mutation=True)
 def get_label_features(features, parents=None):
     if parents is None:
         parents = []
@@ -95,11 +90,13 @@ def get_label_features(features, parents=None):
     return text_features
 
 # Cast info to pure dictionary by casting SplitInfo and pre-selecting features
-def dictionarize_info(info_dict):
+@st.cache(allow_output_mutation=True)
+def dictionarize_info(dset_info):
+    info_dict = asdict(dset_info)
     res = {}
     res["config_name"] = info_dict["config_name"]
     res["splits"] = {
-        spl: spl_info.num_examples
+        spl: spl_info["num_examples"]
         for spl, spl_info in info_dict["splits"].items()
     }
     res["features"] = {
@@ -111,17 +108,18 @@ def dictionarize_info(info_dict):
     return res
 
 # All together now!
-@st.cache(persist=True, allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def get_config_infos_dict(name):
     return {
-        config_info["config_name"]: dictionarize_info(config_info)
-        for config_info in get_config_infos(name)
+        conf_name: dictionarize_info(conf_info)
+        for conf_name, conf_info in get_dataset_infos(name).items()
     }
 
 ####### prepare text and scores
 # extract text to analyze:
 # list of all examples of a given feature in a given split of a given config of a given dataset
 # returns a dataset of strings
+@st.cache(allow_output_mutation=True)
 def extract_text(examples, text_path):
     text_list = []
     example_ct = 0
@@ -139,7 +137,7 @@ def extract_text(examples, text_path):
     ]
     return {"text": text_list}
 
-@st.cache(persist=True, allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def get_text_to_analyze(name, text_path, config, split=None, max_items=20000, streaming=False):
     ### default arguments
     if split is None:
@@ -157,19 +155,58 @@ def get_text_to_analyze(name, text_path, config, split=None, max_items=20000, st
     return dataset_text
 
 ########## metrics code
-tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
-model = AutoModelForCausalLM.from_pretrained("xlnet-base-cased")
-def get_single_sent_loss(sent):
-    batch = tokenizer(sent, return_tensors="pt", padding=True)
-    batch['labels'] = batch['input_ids']
-    loss = model(**batch).loss.item()
-    return loss
+@st.cache(allow_output_mutation=True, hash_funcs={Dataset: lambda _: None})
+def run_tok_length_analysis(text_dset, cache_name):
+    text_dset_lengths = text_dset.map(
+        lambda exple: {"space_tok_length": len(exple["text"].split())},
+        load_from_cache_file=True,
+        cache_file_name=pjoin("cache_dir", f"{cache_name}_space_tok_length"),
+    )
+    hist_data_tok_length = [text_dset_lengths["space_tok_length"]]
+    fig_tok_length = ff.create_distplot(hist_data_tok_length, group_labels=["text lengths"])
+    sorted_sents_lengths = [
+        (l, s) for s, l in sorted(
+            [(sentence["text"], sentence["space_tok_length"]) for sentence in text_dset_lengths],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    return (sorted_sents_lengths, fig_tok_length)
+
+
+device = "cuda:0"
+@st.cache(allow_output_mutation=True, hash_funcs={
+    Dataset: lambda _: None,
+    transformers.models.xlnet.tokenization_xlnet_fast.XLNetTokenizerFast: lambda _: None,
+    transformers.models.xlnet.modeling_xlnet.XLNetLMHeadModel: lambda _: None,
+})
+def run_perplexity_analysis(text_dset, cache_name):
+    tokenizer = transformers.AutoTokenizer.from_pretrained("xlnet-base-cased")
+    model = transformers.AutoModelForCausalLM.from_pretrained("xlnet-base-cased").to(device)
+    def get_single_sent_loss(sent):
+        batch = tokenizer(sent, return_tensors="pt", padding=True)
+        batch['labels'] = batch['input_ids']
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            loss = model(**batch).loss.item()
+            return loss
+    text_dset_loss = text_dset.map(
+        lambda exple: {"xlnet_loss": get_single_sent_loss(exple["text"])},
+        load_from_cache_file=True,
+        cache_file_name=pjoin("cache_dir", f"{cache_name}_xlnet_loss"),
+    )
+    hist_data_loss = [text_dset_loss["xlnet_loss"]]
+    fig_tok_loss = ff.create_distplot(hist_data_loss, group_labels=["text perplexities"])
+    sorted_sents_loss = [
+        (l, s) for s, l in sorted(
+            [(sentence["text"], sentence["xlnet_loss"]) for sentence in text_dset_loss],
+            key=lambda x:x[1], reverse=True,
+        )
+    ]
+    return (sorted_sents_loss, fig_tok_loss)
 
 ########## streamlit code
 
-ds_list = list_datasets(with_community_datasets=False, with_details=True)
-ds_name_to_dict = {ds.id: ds.description for ds in ds_list}
-ds_names = list(ds_name_to_dict.keys())
+ds_names, ds_name_to_dict = all_datasets()
 
 st.title("Data Analysis Tools")
 
@@ -213,13 +250,11 @@ with st.sidebar.expander("Choose first dataset and field"):
         ds_config_a["splits"].keys(),
         index=0,
     )
-    num_examples_a = st.slider(
-        "Number of examples to analyze in the first dataset:",
-        0,
-        min(ds_config_a["splits"][split_a], 50000),
-        value=min(1000, ds_config_a["splits"][split_a]),
-        step=1000,
+    analyze_sample_a = st.checkbox(
+        f"Only analyze the first {_SAMPLE_SIZE} for the first dataset",
+        value=True,
     )
+    num_examples_a = _SAMPLE_SIZE if analyze_sample_a else ds_config_a["splits"][split_a]
     streaming_a = st.checkbox(
         "Use streaming functionality for the first dataset",
         value=False,
@@ -258,13 +293,11 @@ with st.sidebar.expander("Choose second dataset and field"):
         ds_config_b["splits"].keys(),
         index=0,
     )
-    num_examples_b = st.slider(
-        "Number of examples to analyze in the second dataset:",
-        0,
-        min(ds_config_b["splits"][split_b], 50000),
-        value=min(1000, ds_config_b["splits"][split_b]),
-        step=1000,
+    analyze_sample_b = st.checkbox(
+        f"Only analyze the first {_SAMPLE_SIZE} for the second dataset",
+        value=True,
     )
+    num_examples_b = _SAMPLE_SIZE if analyze_sample_b else ds_config_b["splits"][split_b]
     streaming_b = st.checkbox(
         "Use streaming functionality for the second dataset",
         value=False,
@@ -274,6 +307,9 @@ with st.sidebar.expander("Choose second dataset and field"):
         value=False,
     )
 
+# doing some of the caching manually
+cache_name_a = f"{ds_name_a}_{config_name_a}_{split_a}_{'-'.join(text_feature_a)}_{num_examples_a}"
+cache_name_b = f"{ds_name_b}_{config_name_b}_{split_b}_{'-'.join(text_feature_b)}_{num_examples_b}"
 
 # Grab the text requested
 text_dset_a = get_text_to_analyze(
@@ -285,33 +321,6 @@ text_dset_b = get_text_to_analyze(
     split=split_b, max_items=num_examples_b, streaming=streaming_b
 )
 
-cache_name_a = f"{ds_name_a}_{config_name_a}_{split_a}_{'-'.join(text_feature_a)}_{num_examples_a}"
-cache_name_b = f"{ds_name_b}_{config_name_b}_{split_b}_{'-'.join(text_feature_b)}_{num_examples_b}"
-
-text_dset_a_lengths = text_dset_a.map(
-    lambda exple: {"space_tok_length": len(exple["text"].split())},
-    load_from_cache_file=True,
-    cache_file_name=pjoin("cache_dir", f"{cache_name_a}_space_tok_length"),
-)
-text_dset_b_lengths = text_dset_b.map(
-    lambda exple: {"space_tok_length": len(exple["text"].split())},
-    load_from_cache_file=True,
-    cache_file_name=pjoin("cache_dir", f"{cache_name_b}_space_tok_length"),
-)
-
-if compute_perplexities_a:
-    text_dset_a_loss = text_dset_a.map(
-        lambda exple: {"xlnet_loss": get_single_sent_loss(exple["text"])},
-        load_from_cache_file=True,
-        cache_file_name=pjoin("cache_dir", f"{cache_name_a}_xlnet_loss"),
-    )
-if compute_perplexities_b:
-    text_dset_b_loss = text_dset_b.map(
-        lambda exple: {"xlnet_loss": get_single_sent_loss(exple["text"])},
-        load_from_cache_file=True,
-        cache_file_name=pjoin("cache_dir", f"{cache_name_b}_xlnet_loss"),
-    )
-
 ######## Main window
 
 left_col, right_col = st.columns(2)
@@ -321,96 +330,58 @@ left_col.markdown(f"### Showing {ds_name_a} - {config_name_a} - {text_feature_a}
 with left_col.expander("Dataset Description A"):
     st.markdown(ds_name_to_dict[ds_name_a])
 
-with left_col.expander("Show some examples A"):
-    st.markdown("### Example text fields A")
-    start_id_show = st.slider('Starting index A:', 0, text_dset_a.num_rows - 5, value=0, step=5)
-    st.dataframe(text_dset_a[start_id_show:start_id_show+10]["text"])
-
 right_col.markdown(f"### Showing {ds_name_b} - {config_name_b} - {text_feature_b}")
 with right_col.expander("Dataset Description B"):
     st.markdown(ds_name_to_dict[ds_name_b])
 
-with right_col.expander("Show some examples B"):
-    st.markdown("### Example text fields B")
-    start_id_show = st.slider('Starting index B:', 0, text_dset_b.num_rows - 10, 5)
-    st.dataframe(text_dset_b[start_id_show:start_id_show+10]["text"])
-
 ### Second, show the distribution of text lengths
 with left_col.expander("Show text lengths A", expanded=True):
     st.markdown("### Text lengths A")
-    hist_data_tok_length_a = [text_dset_a_lengths["space_tok_length"]]
-    fig_tok_length_a = ff.create_distplot(hist_data_tok_length_a, group_labels=["text lengths"])
+    sorted_sents_lengths_a, fig_tok_length_a = run_tok_length_analysis(text_dset_a, cache_name_a)
     st.plotly_chart(fig_tok_length_a, use_container_width=True)
-    sorted_sents_lengths_a = [
-        s for s, l in sorted(
-            [(sentence["text"], sentence["space_tok_length"]) for sentence in text_dset_a_lengths],
-            key=lambda x:x[1], reverse=True,
-        )
-    ]
     start_id_show_lengths_a = st.slider(
         'Show longest sentences in A starting at index:',
         0, text_dset_a.num_rows - 5, value=0, step=5
     )
-    for sent in sorted_sents_lengths_a[start_id_show_lengths_a:start_id_show_lengths_a+5]:
-        st.text(sent)
+    for ln, sent in sorted_sents_lengths_a[start_id_show_lengths_a:start_id_show_lengths_a+5]:
+        st.text(f"{ln} | {sent}")
 
 with right_col.expander("Show text lengths B", expanded=True):
     st.markdown("### Text lengths B")
-    hist_data_tok_length_b = [text_dset_b_lengths["space_tok_length"]]
-    fig_tok_length_b = ff.create_distplot(hist_data_tok_length_b, group_labels=["text lengths"])
+    sorted_sents_lengths_b, fig_tok_length_b = run_tok_length_analysis(text_dset_b, cache_name_b)
     st.plotly_chart(fig_tok_length_b, use_container_width=True)
-    sorted_sents_lengths_b = [
-        s for s, l in sorted(
-            [(sentence["text"], sentence["space_tok_length"]) for sentence in text_dset_b_lengths],
-            key=lambda x:x[1], reverse=True,
-        )
-    ]
     start_id_show_lengths_b = st.slider(
         'Show longest sentences in B starting at index:',
-        0, text_dset_a.num_rows - 5, value=0, step=5
+        0, text_dset_b.num_rows - 5, value=0, step=5
     )
-    for sent in sorted_sents_lengths_b[start_id_show_lengths_b:start_id_show_lengths_b+5]:
-        st.text(sent)
+    for ln, sent in sorted_sents_lengths_b[start_id_show_lengths_b:start_id_show_lengths_b+5]:
+        st.text(f"{ln} | {sent}")
 
 ### Third, show the distribution of text perplexities
 with left_col.expander("Show text perplexities A", expanded=True):
     if compute_perplexities_a:
         st.markdown("### Text perplexities A")
-        hist_data_loss_a = [text_dset_a_loss["xlnet_loss"]]
-        fig_loss_a = ff.create_distplot(hist_data_loss_a, group_labels=["text perplexity"])
+        sorted_sents_loss_a, fig_loss_a = run_perplexity_analysis(text_dset_a, cache_name_a)
         st.plotly_chart(fig_loss_a, use_container_width=True)
-        sorted_sents_loss_a = [
-            s for s, l in sorted(
-                [(sentence["text"], sentence["xlnet_loss"]) for sentence in text_dset_a_loss],
-                key=lambda x:x[1], reverse=True,
-            )
-        ]
         start_id_show_loss_a = st.slider(
             'Show highest perplexity sentences in A starting at index:',
             0, text_dset_a.num_rows - 5, value=0, step=5
         )
-        for sent in sorted_sents_loss_a[start_id_show_loss_a:start_id_show_loss_a+5]:
-            st.text(sent)
+        for lss, sent in sorted_sents_loss_a[start_id_show_loss_a:start_id_show_loss_a+5]:
+            st.text(f"{lss:.3f} {sent}")
     else:
         st.write("To show perplexity of examples, check the `compute perplexities for the first dataset` box left")
 
 with right_col.expander("Show text perplexities B", expanded=True):
     if compute_perplexities_b:
         st.markdown("### Text perplexities B")
-        hist_data_loss_b = [text_dset_b_loss["xlnet_loss"]]
-        fig_loss_b = ff.create_distplot(hist_data_loss_b, group_labels=["text perplexity"])
+        sorted_sents_loss_b, fig_loss_b = run_perplexity_analysis(text_dset_b, cache_name_b)
         st.plotly_chart(fig_loss_b, use_container_width=True)
-        sorted_sents_loss_b = [
-            s for s, l in sorted(
-                [(sentence["text"], sentence["xlnet_loss"]) for sentence in text_dset_b_loss],
-                key=lambda x:x[1], reverse=True,
-            )
-        ]
         start_id_show_loss_b = st.slider(
             'Show highest perplexity sentences in B starting at index:',
             0, text_dset_b.num_rows - 5, value=0, step=5
         )
-        for sent in sorted_sents_loss_b[start_id_show_loss_b:start_id_show_loss_b+5]:
-            st.text(sent)
+        for lss, sent in sorted_sents_loss_b[start_id_show_loss_b:start_id_show_loss_b+5]:
+            st.text(f"{lss:.3f} {sent}")
     else:
-        st.write("To show perplexity of examples, check the `compute perplexities for the second dataset` box left")
+        st.write("To show perplexity of examples, check the `compute perplexities for the first dataset` box left")
