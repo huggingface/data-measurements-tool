40f072f34f05dc217beb303336c4f333b0eaa819
meg-huggingface <90473723+meg-huggingface@users.noreply.github.com>
2021-10-05 14:38:13 -0700

    Adding zipf calculations

--- data_metrics_collab.py
+++ data_metrics_collab.py
@@ -4,24 +4,29 @@ import statistics
 import sys
 import textwrap
 from collections import Counter
-from typing import Dict
+from typing import Dict, List
 
+import matplotlib.pyplot as plt
 import nltk
-import numpy as np
-import pandas as pd
 import torch
 import yaml
 # If you don't have this installed, see https://huggingface.co/docs/datasets/installation.html
-from datasets import load_dataset, Dataset
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
-from sklearn.feature_extraction.text import CountVectorizer
 # See https://huggingface.co/transformers/installation.html
 # Used from loading pretrained models and tokenizers
 from transformers import AutoTokenizer, AutoModelWithLMHead, \
     AutoModelForMaskedLM
+
+plt.style.use('fivethirtyeight')
+import powerlaw
+import pandas as pd
+import numpy as np
+from datasets import load_dataset
+from sklearn.feature_extraction.text import CountVectorizer
+from scipy.stats import ks_2samp, zipf
 
 # See http://docs.allennlp.org/main/api/fairness/bias_metrics/#associationwithoutgroundtruth
 
@@ -61,6 +66,9 @@ parser.add_argument('--language-column', type=str, required=True,
 parser.add_argument('--clean-html', default=False, required=False,
                     action="store_true",
                     help='Whether to clear out HTML in the text before processing (Optional)')
+parser.add_argument('--streaming', default=False, required=False,
+                    action="store_true",
+                    help='Whether to stream data in (Optional)')
 
 args = parser.parse_args()
 
@@ -225,23 +233,23 @@ def do_clean_html(raw_html: str) -> str:
 
 
 # Dataset Characteristics
-def get_data_basics(input_dataset: Dataset, label_column_name: str,
+def get_data_basics(input_dataset, label_column_name: str,
                     label_type: str) -> Dict:
     # Should we ask about deduping?!
     """
     # Takes a DatasetDict & isolates the Dataset of interest as a dataframe using json_normalize
     # on the value of the relevant Dataset key (dataset_column_name).
     # We will need to know this Dataset key name from a config file.
-    :type input_dataset: Dataset
+    :type input_dataset: DataFrame
     :type label_column_name: str
     :type label_type: str
     :rtype: Dict
     """
     basics_dict = {}
-    num_rows = input_dataset.num_rows
     # Turn the Dataset into a data frame, using json_normalize so that the Dataset, too, will be a data frame.
     # Note that json_normalize is preferable to from_dict for handling of nested dicts.
-    df = pd.json_normalize(input_dataset)
+    # df = pd.json_normalize(input_dataset)
+    df = input_dataset
     # TODO: .info() as the summarization mechanism, as well as .isnull() to get a view of the NaN
     if VERBOSE:
         print('\n* Step 1: Peek at data. Calculating dataset characteristics.')
@@ -253,7 +261,6 @@ def get_data_basics(input_dataset: Dataset, label_column_name: str,
     # If not, then we will need to use shape,
     # And it will be a smaller number of rows when streaming into here.
     data_shape = df.shape
-    assert (data_shape[0] == num_rows)
     basics_dict['num_rows'] = data_shape[0]
     basics_dict['num_cols'] = data_shape[1]
     if label_column_name:
@@ -276,8 +283,98 @@ def get_data_basics(input_dataset: Dataset, label_column_name: str,
     return basics_dict
 
 
+def rank_words(term_df):
+    # Put the right rank for each word. Words with the same count are in the same rank.
+    # Save the words in each rank in a dict, used later when plotting it out.
+    ranked_words = {}
+    unique_counts = pd.unique(term_df['count'])
+    unique_ranks = np.arange(1, len(unique_counts) + 1)
+
+    for count, rank in zip(unique_counts, unique_ranks):
+        term_df.loc[term_df['count'] == count, 'rank'] = rank
+        ranked_words[rank] = ','.join(term_df[term_df['count'] == count].index)
+    return term_df, ranked_words
+
+
+def test_fit(fit):
+    print(
+        "Checking log likelihood ratio to see if the data is better explained")
+    print("by other well-behaved distributions...")
+    # The first value returned from distribution_compare is the log likelihood ratio
+    better_distro = False
+    trunc = fit.distribution_compare('power_law', 'truncated_power_law')
+    if trunc[0] < 0:
+        print("Seems a truncated power law is a better fit.")
+        better_distro = True
+
+    lognormal = fit.distribution_compare('power_law', 'lognormal')
+    if lognormal[0] < 0:
+        print("Seems a lognormal distribution is a better fit.")
+        print("But don't panic -- that happens sometimes with language.")
+        better_distro = True
+
+    exponential = fit.distribution_compare('power_law', 'exponential')
+    if exponential[0] < 0:
+        print("Seems an exponential distribution is a better fit. Panic.")
+        better_distro = True
+
+    if not better_distro:
+        print("\nSeems your data is best fit by a power law. Celebrate!!")
+
+
+def do_zipf_pmf(term_df, xmin, alpha):
+    # The fit is based on an optimal xmin (minimum rank)
+    # Let's use this to make count estimates for the zipf pmf
+    pmf_mass = float(sum(pd.unique(term_df[term_df['rank'] > xmin]['count'])))
+    # Note that the pmf without the xmin accounting should sum to 1.
+    zipf_pmf = np.array([int(round(zipf.pmf(p, alpha) * pmf_mass)) for p in
+                         pd.unique(term_df['rank'])])
+
+
+def fit_data(term_df):
+    # Uses the powerlaw package to fit the observed frequencies to a zipfian distribution
+    observed_counts = term_df['count'].values
+    # 'fit_method' is MLE by default; doesn't seem to change the results in my initial pokings.
+    # Also tried discrete_approximation="xmax"
+    # Note another method for determining alpha
+    # might be defined by (Newman, 2005 for details): alpha = 1 + n * sum(ln( xi / xmin )) ^ -1
+    fit = powerlaw.Fit(observed_counts, fit_method="KS", discrete=True)
+    # Returns:
+    #     pdf_bin_edges = The portion of the data that is within the bin.
+    #     observed_pdf = The probability density function (normalized histogram) of the data.
+    # This should probably be a pmf, not a pdf. But perhaps using discrete=True above helps.
+    # Setting original_data to False uses only the data used for the fit (within xmin and xmax).
+    pdf_bin_edges, observed_pdf = fit.pdf(original_data=False)
+    # Descending, not ascending
+    observed_pdf = np.flip(observed_pdf)
+    pdf_bin_edges = np.flip(pdf_bin_edges)
+    # This seems to basically be the 'Distribution' class described here: https://pythonhosted.org/powerlaw/#powerlaw.Fit.pdf
+    theoretical_distribution = fit.power_law
+    # The likelihoods of the observed data from the theoretical distribution.
+    predicted_likelihoods = theoretical_distribution.likelihoods
+    # The logarithm of the likelihoods of the observed data from the theoretical distribution.
+    predicted_log_likelihoods = theoretical_distribution.loglikelihoods
+    # The probability density function (normalized histogram) of the theoretical distribution
+    predicted_pdf = theoretical_distribution.pdf()
+    # Descending, not ascending
+    predicted_pdf = np.flip(predicted_pdf)
+    # !!!! CRITICAL VALUE FOR ZIPF !!!!
+    alpha = theoretical_distribution.alpha
+    # The optimal xmin *beyond which* the scaling regime of the power law fits best. (This means exclusive xmin, right?)
+    xmin = theoretical_distribution.xmin
+    xmax = theoretical_distribution.xmax
+    distance = theoretical_distribution.KS()
+    print("------------------------------------------------")
+    print("Optimal alpha:\t\t%.4f" % alpha)
+    print("Optimal first rank:\t%s" % xmin)
+    print("Optimal last rank:\t%s" % xmax)
+    print("Distance:\t\t%.4f" % distance)
+    return alpha, xmin, xmax, distance, observed_pdf, predicted_pdf
+
+
 # Vocabulary Size
-def get_count_vocab(input_dataset: Dataset, langa_column_name: str, lower=True,
+def get_count_vocab(input_dataset, langa_column_name: str,
+                    lower=True,
                     language="english", clean_html=False) \
         -> Dict:
     vocab_dict = {}
@@ -287,7 +384,7 @@ def get_count_vocab(input_dataset: Dataset, langa_column_name: str, lower=True,
     # TODO: A single ID will have multiple duplicated sources if there are multiple translations.
     # So yeah. We have to deal with that.
     # Turn the Dataset into a data frame.
-    df = pd.DataFrame.from_dict(input_dataset)
+    df = input_dataset  # pd.DataFrame.from_dict(input_dataset)
     # Counts the number of tokens, with or without lowercase normalization.
     if VERBOSE:
         print('\n* Step 2: Calculating statistics on text looking like this.')
@@ -353,7 +450,51 @@ def get_count_vocab(input_dataset: Dataset, langa_column_name: str, lower=True,
 # Closed and open pi chart?
 # Find the closed word lists for the different languages in order to calculate distributional statistics.
 
-def count_vocab_frequencies(term_df):
+def do_zipf(input_dataset, langa_column_name):
+    term_df = count_vocab_frequencies(input_dataset,
+                                      langa_column_name=langa_column_name)
+    term_df['proportion'] = term_df['count'] / float(sum(term_df['count']))
+    rank_column = term_df['count'].rank(method='dense', numeric_only=True,
+                                        ascending=False)
+    term_df['rank'] = rank_column.astype('int64')
+    print("Info on the observed frequencies:")
+    print(term_df.info())
+    print("------------------------------------")
+    print("Vocab size (types):\t%s" % len(term_df))
+    print("Vocab size (tokens):\t%s" % sum(term_df['count']))
+    print("Observations look like this:")
+    print(term_df.head())
+    print('...')
+    print(term_df.tail())
+    alpha, xmin, xmax, distance, observed_pdf, predicted_pdf = fit_data(term_df)
+    # Significance testing
+    # Note: We may want to use bootstrapping (instead of the standard KS test p-value tables) to determine statistical significance
+    # See: https://stats.stackexchange.com/questions/264431/how-to-determine-if-zipfs-law-can-be-applied Answer #4
+    print("Checking the goodness of fit of our observed distribution")
+    print(" to the hypothesized power law distribution")
+    print(" using a Kolmogorov–Smirnov (KS) test.")
+    ks_test = ks_2samp(observed_pdf, predicted_pdf)
+    # print("KS test:", end='\t\t')
+    print(ks_test)
+    print("\nThe KS test p-value is: %.4f" % ks_test.pvalue)
+    if ks_test.pvalue < .01:
+        print(
+            "\nYour data fits a powerlaw with a minimum KS distance of %.4f" % distance)
+        print("\nWoohoo!")
+    else:
+        print("\nYour data does not fits a powerlaw. =(")
+        print("\nDO BETTER.")
+    term_df, ranked_words = rank_words(term_df)
+    if not xmax:
+        xmax = len(ranked_words)
+    zipf_dict = {'alpha': float(alpha), 'xmin': float(xmin), 'xmax': float(
+        xmax),
+                 'distance': float(distance), 'pvalue': float(ks_test.pvalue)}
+    print(zipf_dict)
+    return zipf_dict, term_df, ranked_words
+
+
+def count_vocab_frequencies(term_df, langa_column_name):
     """
     Based on an input pandas DataFrame with a 'text' column,
     this function will count the occurrences of ALL words
@@ -361,6 +502,7 @@ def count_vocab_frequencies(term_df):
     with the rows corresponding to the different vocabulary words
     and the column to the total count of that word.
     """
+    term_df = term_df[langa_column_name]
     cvec = CountVectorizer(token_pattern=u"(?u)\\b\\w+\\b")
     # Needed to modify the minimum token length :
     # https://stackoverflow.com/questions/33260505/countvectorizer-ignoring-i
@@ -376,30 +518,36 @@ def count_vocab_frequencies(term_df):
         i += 1
     term_freq_df = pd.DataFrame([np.sum(tf, axis=0)],
                                 columns=cvec.get_feature_names()).transpose()
-    term_freq_df.columns = ['total']
-    return term_freq_df.nlargest(10, 'total')
+    term_freq_df.columns = ['count']
+    term_freq_df.index.name = 'word'
+    sorted_term_freq_df = pd.DataFrame(
+        term_freq_df.sort_values(by='count', ascending=False)['count'])
+    return sorted_term_freq_df
 
 
-def get_zipf(input_dataset, langa_column_name):
+"""
+
+def do_zipf(input_dataset, langa_column_name):
     # Turn the Dataset into a data frame.
-    df = pd.DataFrame.from_dict(input_dataset)
+    df = input_dataset #pd.DataFrame.from_dict(input_dataset)
     term_df = count_vocab_frequencies(df[langa_column_name])
     term_freq_df = count_vocab_frequencies(term_df)
     # Real vs. Projected value according to Zipf; flag those that are a difference higher than x
     # # for what's predicted by the law.
     # For closed class words, what could we say about that means semantically?
     return term_freq_df.to_dict()
+"""
 
 
 # Instance Characteristics
-def get_text_stats(input_dataset: Dataset, langa_column_name: str) -> Dict:
+def get_text_stats(input_dataset, langa_column_name: str) -> Dict:
     # Calculates sufficient statistics for text-based instances: average, mean, median
     total_lens = 0
     all_lengths = []
     text_dict = {}
     i = 1
     # Turn the Dataset into a data frame.
-    df = pd.DataFrame.from_dict(input_dataset)
+    df = input_dataset
     if VERBOSE:
         print("\n* Step 3: Get text stats. Text is looking like this.")
         print(df.head())
@@ -427,29 +575,53 @@ def do_dataset(dataset_name: str, config_name: str, dataset_split_name: str,
                label_column_name: str,
                label_type="discrete", langa_column_name="text", lower=True,
                language="english",
-               clean_html=False) -> Dict:
-    data_dict = load_dataset(dataset_name, config_name)
-    desired_dataset = data_dict[dataset_split_name]
-    data_basics_dict = get_data_basics(desired_dataset,
+               clean_html=False, streaming=False) -> Dict:
+    # Dataset object
+    loaded_dataset = load_dataset(dataset_name, config_name,
+                                  split=dataset_split_name, streaming=streaming)
+    # For now, 'streaming' just means we're taking some first 5000 chunks.
+    n = 5000
+    # TODO: Json normalize for both?
+    if streaming:
+        data_head = loaded_dataset.take(n)
+        df = pd.DataFrame(data_head)
+    else:
+        df = pd.json_normalize(loaded_dataset)
+    # desired_dataset = data_dict[dataset_split_name]
+    data_basics_dict = get_data_basics(input_dataset=df,
                                        label_column_name=label_column_name,
                                        label_type=label_type)
     # Want to do this for both *source* and *target*
-    data_vocab_dict = get_count_vocab(input_dataset=desired_dataset,
+    data_vocab_dict = get_count_vocab(input_dataset=df,
                                       langa_column_name=langa_column_name,
                                       lower=lower,
                                       language=language, clean_html=clean_html)
-    data_text_dict = get_text_stats(desired_dataset,
+    data_text_dict = get_text_stats(input_dataset=df,
                                     langa_column_name=langa_column_name)
     ppl_dict = get_perplexity(dataset_name, config_name, dataset_split_name,
                               langa_column_name)
-    zipf_dict = get_zipf(input_dataset=desired_dataset,
-                         langa_column_name=langa_column_name)
+    zipf_dict, term_df, ranked_words = do_zipf(input_dataset=df,
+                                               langa_column_name=langa_column_name)
     # TODO: Run all the rest of the metrics
     output_yaml_data = {"Basic Data Characteristics": data_basics_dict,
                         "Vocab Characteristics": data_vocab_dict,
                         "Text Characteristics": data_text_dict,
                         "Perplexity": ppl_dict, "Zipf distribution": zipf_dict}
     return output_yaml_data
+
+
+def do_c4_dataset() -> Dict:
+    # data= load_dataset("oscar", "unshuffled_deduplicated_en", split = "train", streaming= True)
+    #data = load_dataset("c4", "en", split="train", streaming=True)
+    #grab_n = 5000
+    # For streaming data
+    #print('Note: Just taking the first %s instances.' % grab_n)
+    #data_head = data.take(grab_n)
+    #df = pd.DataFrame(data_head)
+    c4_yaml = do_dataset(dataset_name="c4", config_name="en",
+                         dataset_split_name="train",
+                         langa_column_name="text", streaming=True)
+    return c4_yaml
 
 
 def do_glue_ax_dataset() -> Dict:
@@ -489,7 +661,7 @@ def do_imdb_train_dataset() -> Dict:
 # TODO: Get dataset size to help solve whether to stream of read in full.
 # (could be config, could be automatically grabbed)
 
-def main(args) -> Dict:
+def main(args) -> List:
     dataset_name = args.dataset
     config_name = args.config
     dataset_split_name = args.split
@@ -497,6 +669,7 @@ def main(args) -> Dict:
     label_type = args.label_type
     langa_column_name = args.language_column
     clean_html = args.clean_html
+    streaming = args.streaming
     # This is going to need to expand as the other options will be necessary to distinguish between, e.g,
     # different splits of the dataset being analyzed.
     yaml_name = dataset_name + "-" + config_name + ".yaml"
@@ -506,7 +679,8 @@ def main(args) -> Dict:
                              label_column_name=label_column_name,
                              label_type=label_type,
                              langa_column_name=langa_column_name,
-                             clean_html=clean_html)
+                             clean_html=clean_html,
+                             streaming=streaming)
     write_yaml(output_yaml, yaml_name)
     # TODO: Dump a json for the lists of ranked words, etc.; this can be used for visualization work.
     return [yaml_name]
@@ -564,4 +738,34 @@ def what_is_this_used_for_now:
     
     for i, test in enumerate(test_data):
         print("PP({0}):{1}".format(test_sentences[i], model.perplexity(test)))
+        
+# Just stashing it here so I can use a different version
+def count_vocab_frequencies(df, cutoff=0):
+    \"\"\"
+    Based on an input pandas DataFrame with a 'text' column,
+    this function will count the occurrences of all words
+    with a frequency higher than 'cutoff' and will return another DataFrame
+    with the rows corresponding to the different vocabulary words
+    and the column to the count count of that word.
+    \"\"\"
+    # Move this up as a constant in larger code.
+    batch_size = 10
+    cvec = CountVectorizer(token_pattern=u"(?u)\\b\\w+\\b")
+    # Needed to modify the minimum token length:
+    # https://stackoverflow.com/questions/33260505/countvectorizer-ignoring-i
+    cvec.fit(df.text)
+    document_matrix = cvec.transform(df.text)
+    batches = np.linspace(0, df.shape[0], batch_size).astype(int)
+    i = 0
+    tf = []
+    while i < len(batches) - 1:
+        batch_result = np.sum(document_matrix[batches[i]:batches[i+1]].toarray(), axis=0)
+        tf.append(batch_result)
+        i += 1
+    term_freq_df = pd.DataFrame([np.sum(tf, axis=0)], columns=cvec.get_feature_names()).transpose()
+    term_freq_df.columns = ['count']
+    term_freq_df.index.name = 'word'
+    term_freq_df = term_freq_df[term_freq_df['count'] > cutoff]
+    sorted_term_freq_df = pd.DataFrame(term_freq_df.sort_values(by='count', ascending=False)['count'])
+    return sorted_term_freq_df
 """
