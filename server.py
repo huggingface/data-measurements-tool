269580190ab9083e90b8f5f5ce7e5930fcabfa28
Tristan Thrush <tristan@huggingface.co>
2022-05-11 13:13:15 -0700

    added command printout to server

--- server.py
+++ server.py
@@ -18,11 +18,12 @@ class handler(BaseHTTPRequestHandler):
         message = "success"
         self.wfile.write(bytes(message, "utf8"))
 
-        print('recieved message', dataset_args)
+        print('recieved message:', dataset_args)
 
         command = "python3 run_data_measurements.py --dataset=" + dataset_args["dset_name"][0] + " --config=" + dataset_args["dset_config"][0] + " --split=" + dataset_args["split_name"][0] + " --feature=" + dataset_args["text_field"][0] + " --email=" + dataset_args["email"][0]
         if "label_field" in dataset_args and len(dataset_args["label_field"]) > 0:
             command += " --label_field=" + dataset_args["label_field"][0]
+        print('running command:', command)
         subprocess.Popen(command, shell=True)
 
 with HTTPServer(('', 80), handler) as server:
