d01ae194655d4e6ba53533088a8f542d63778ab4
meg-huggingface <90473723+meg-huggingface@users.noreply.github.com>
2022-08-24 18:27:20 -0700

    In progress

--- data_measurements/dataset_statistics.py
+++ data_measurements/dataset_statistics.py
@@ -729,6 +729,18 @@ class DatasetStatisticsCacheClass:
                                                    use_cache=self.use_cache)
         self.npmi_stats.load_or_prepare_npmi_terms()
 
+
+
+
+        npmi_obj = npmi.DMTHelper(self, save=save)
+        npmi_obj.run_DMT_processing(list_duplicates=list_duplicates)
+        self.npmi_results = npmi_obj.results
+        self.npmi_frac = self.duplicates_results[td.DUPS_FRAC]
+        if list_duplicates and td.DUPS_DICT in self.duplicates_results:
+            self.npmi_dict = self.duplicates_results[td.DUPS_DICT]
+        self.duplicates_files = npmi_obj.get_duplicates_filenames()
+
+
     def load_or_prepare_zipf(self, save=True):
         if self.use_cache:
             zipf_json_fid, zipf_fig_json_fid, zipf_fig_html_fid = zipf.get_zipf_fids(
@@ -804,238 +816,6 @@ def _set_idx_cols_from_cache(csv_df, subgroup=None, calc_str=None):
     return csv_df
 
 
-class nPMIStatisticsCacheClass:
-    """ "Class to interface between the app and the nPMI class
-    by calling the nPMI class with the user's selections."""
-
-    def __init__(self, dataset_stats, use_cache=False):
-        self.live = dataset_stats.live
-        self.dstats = dataset_stats
-        self.pmi_cache_path = pjoin(self.dstats.cache_path, "pmi_files")
-        if not isdir(self.pmi_cache_path):
-            logs.warning(
-                "Creating pmi cache directory %s." % self.pmi_cache_path)
-            # We need to preprocess everything.
-            mkdir(self.pmi_cache_path)
-        self.joint_npmi_df_dict = {}
-        # TODO: Users ideally can type in whatever words they want.
-        self.termlist = _IDENTITY_TERMS
-        # termlist terms that are available more than _MIN_VOCAB_COUNT times
-        self.available_terms = _IDENTITY_TERMS
-        logs.info(self.termlist)
-        self.use_cache = use_cache
-        # TODO: Let users specify
-        self.open_class_only = True
-        self.min_vocab_count = self.dstats.min_vocab_count
-        self.subgroup_files = {}
-        self.npmi_terms_fid = pjoin(self.dstats.cache_path, "npmi_terms.json")
-
-    def load_or_prepare_npmi_terms(self):
-        """
-        Figures out what identity terms the user can select, based on whether
-        they occur more than self.min_vocab_count times
-        :return: Identity terms occurring at least self.min_vocab_count times.
-        """
-        # TODO: Add the user's ability to select subgroups.
-        # TODO: Make min_vocab_count here value selectable by the user.
-        if (
-                self.use_cache
-                and exists(self.npmi_terms_fid)
-                and json.load(open(self.npmi_terms_fid))[
-            "available terms"] != []
-        ):
-            available_terms = json.load(open(self.npmi_terms_fid))[
-                "available terms"]
-        else:
-            true_false = [
-                term in self.dstats.vocab_counts_df.index for term in
-                self.termlist
-            ]
-            word_list_tmp = [x for x, y in zip(self.termlist, true_false) if y]
-            true_false_counts = [
-                self.dstats.vocab_counts_df.loc[
-                    word, CNT] >= self.min_vocab_count
-                for word in word_list_tmp
-            ]
-            available_terms = [
-                word for word, y in zip(word_list_tmp, true_false_counts) if y
-            ]
-            logs.info(available_terms)
-            with open(self.npmi_terms_fid, "w+") as f:
-                json.dump({"available terms": available_terms}, f)
-        self.available_terms = available_terms
-        return available_terms
-
-    def load_or_prepare_joint_npmi(self, subgroup_pair):
-        """
-        Run on-the fly, while the app is already open,
-        as it depends on the subgroup terms that the user chooses
-        :param subgroup_pair:
-        :return:
-        """
-        # Canonical ordering for subgroup_list
-        subgroup_pair = sorted(subgroup_pair)
-        subgroup1 = subgroup_pair[0]
-        subgroup2 = subgroup_pair[1]
-        subgroups_str = "-".join(subgroup_pair)
-        if not isdir(self.pmi_cache_path):
-            logs.warning("Creating cache")
-            # We need to preprocess everything.
-            # This should eventually all go into a prepare_dataset CLI
-            mkdir(self.pmi_cache_path)
-        joint_npmi_fid = pjoin(self.pmi_cache_path, subgroups_str + "_npmi.csv")
-        subgroup_files = define_subgroup_files(subgroup_pair,
-                                               self.pmi_cache_path)
-        # Defines the filenames for the cache files from the selected subgroups.
-        # Get as much precomputed data as we can.
-        if self.use_cache and exists(joint_npmi_fid):
-            # When everything is already computed for the selected subgroups.
-            logs.info("Loading cached joint npmi")
-            joint_npmi_df = self.load_joint_npmi_df(joint_npmi_fid)
-            npmi_display_cols = [
-                "npmi-bias",
-                subgroup1 + "-npmi",
-                subgroup2 + "-npmi",
-                subgroup1 + "-count",
-                subgroup2 + "-count",
-            ]
-            joint_npmi_df = joint_npmi_df[npmi_display_cols]
-            # When maybe some things have been computed for the selected subgroups.
-        else:
-            if not self.live:
-                logs.info("Preparing new joint npmi")
-                joint_npmi_df, subgroup_dict = self.prepare_joint_npmi_df(
-                    subgroup_pair, subgroup_files
-                )
-                # Cache new results
-                logs.info("Writing out.")
-                for subgroup in subgroup_pair:
-                    write_subgroup_npmi_data(subgroup, subgroup_dict,
-                                             subgroup_files)
-                with open(joint_npmi_fid, "w+") as f:
-                    joint_npmi_df.to_csv(f)
-            else:
-                joint_npmi_df = pd.DataFrame()
-        logs.info("The joint npmi df is")
-        logs.info(joint_npmi_df)
-        return joint_npmi_df
-
-    @staticmethod
-    def load_joint_npmi_df(joint_npmi_fid):
-        """
-        Reads in a saved dataframe with all of the paired results.
-        :param joint_npmi_fid:
-        :return: paired results
-        """
-        with open(joint_npmi_fid, "rb") as f:
-            joint_npmi_df = pd.read_csv(f)
-        joint_npmi_df = _set_idx_cols_from_cache(joint_npmi_df)
-        return joint_npmi_df.dropna()
-
-    def prepare_joint_npmi_df(self, subgroup_pair, subgroup_files):
-        """
-        Computs the npmi bias based on the given subgroups.
-        Handles cases where some of the selected subgroups have cached nPMI
-        computations, but other's don't, computing everything afresh if there
-        are not cached files.
-        :param subgroup_pair:
-        :return: Dataframe with nPMI for the words, nPMI bias between the words.
-        """
-        subgroup_dict = {}
-        # When npmi is computed for some (but not all) of subgroup_list
-        for subgroup in subgroup_pair:
-            logs.info("Load or failing...")
-            # When subgroup npmi has been computed in a prior session.
-            cached_results = self.load_or_fail_cached_npmi_scores(
-                subgroup, subgroup_files[subgroup]
-            )
-            # If the function did not return False and we did find it, use.
-            if cached_results:
-                # FYI: subgroup_cooc_df, subgroup_pmi_df, subgroup_npmi_df = cached_results
-                # Holds the previous sessions' data for use in this session.
-                subgroup_dict[subgroup] = cached_results
-        logs.info("Calculating for subgroup list")
-        joint_npmi_df, subgroup_dict = self.do_npmi(subgroup_pair,
-                                                    subgroup_dict)
-        return joint_npmi_df.dropna(), subgroup_dict
-
-    # TODO: Update pairwise assumption
-    def do_npmi(self, subgroup_pair, subgroup_dict):
-        """
-        Calculates nPMI for given identity terms and the nPMI bias between.
-        :param subgroup_pair: List of identity terms to calculate the bias for
-        :return: Subset of data for the UI
-        :return: Selected identity term's co-occurrence counts with
-                 other words, pmi per word, and nPMI per word.
-        """
-        logs.info("Initializing npmi class")
-        npmi_obj = self.set_npmi_obj()
-        # Canonical ordering used
-        subgroup_pair = tuple(sorted(subgroup_pair))
-        # Calculating nPMI statistics
-        for subgroup in subgroup_pair:
-            # If the subgroup data is already computed, grab it.
-            # TODO: Should we set idx and column names similarly to how we set them for cached files?
-            if subgroup not in subgroup_dict:
-                logs.info("Calculating statistics for %s" % subgroup)
-                vocab_cooc_df, pmi_df, npmi_df = npmi_obj.calc_metrics(subgroup)
-                # Store the nPMI information for the current subgroups
-                subgroup_dict[subgroup] = (vocab_cooc_df, pmi_df, npmi_df)
-        # Pair the subgroups together, indexed by all words that
-        # co-occur between them.
-        logs.info("Computing pairwise npmi bias")
-        paired_results = npmi_obj.calc_paired_metrics(subgroup_pair,
-                                                      subgroup_dict)
-        UI_results = make_npmi_fig(paired_results, subgroup_pair)
-        return UI_results, subgroup_dict
-
-    def set_npmi_obj(self):
-        """
-        Initializes the nPMI class with the given words and tokenized sentences.
-        :return:
-        """
-        # TODO(meg): Incorporate this from evaluate library.
-        # npmi_obj = evaluate.load('npmi', module_type='measurement').compute(subgroup, vocab_counts_df = self.dstats.vocab_counts_df, tokenized_counts_df=self.dstats.tokenized_df)
-        npmi_obj = nPMI(self.dstats.vocab_counts_df, self.dstats.tokenized_df)
-        return npmi_obj
-
-    @staticmethod
-    def load_or_fail_cached_npmi_scores(subgroup, subgroup_fids):
-        """
-        Reads cached scores from the specified subgroup files
-        :param subgroup: string of the selected identity term
-        :return:
-        """
-        # TODO: Ordering of npmi, pmi, vocab triple should be consistent
-        subgroup_npmi_fid, subgroup_pmi_fid, subgroup_cooc_fid = subgroup_fids
-        if (
-                exists(subgroup_npmi_fid)
-                and exists(subgroup_pmi_fid)
-                and exists(subgroup_cooc_fid)
-        ):
-            logs.info("Reading in pmi data....")
-            with open(subgroup_cooc_fid, "rb") as f:
-                subgroup_cooc_df = pd.read_csv(f)
-            logs.info("pmi")
-            with open(subgroup_pmi_fid, "rb") as f:
-                subgroup_pmi_df = pd.read_csv(f)
-            logs.info("npmi")
-            with open(subgroup_npmi_fid, "rb") as f:
-                subgroup_npmi_df = pd.read_csv(f)
-            subgroup_cooc_df = _set_idx_cols_from_cache(
-                subgroup_cooc_df, subgroup, "count"
-            )
-            subgroup_pmi_df = _set_idx_cols_from_cache(
-                subgroup_pmi_df, subgroup, "pmi"
-            )
-            subgroup_npmi_df = _set_idx_cols_from_cache(
-                subgroup_npmi_df, subgroup, "npmi"
-            )
-            return subgroup_cooc_df, subgroup_pmi_df, subgroup_npmi_df
-        return False
-
-    def get_available_terms(self):
-        return self.load_or_prepare_npmi_terms()
 
 
 def dummy(doc):
