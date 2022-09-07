354737440ee1b5dfe8407b6d5d5edc61aa926772
Tristan Thrush <tristan@huggingface.co>
2022-09-06 22:50:38 -0700

    fixed broken merge

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,31 +1,30 @@
 import logging
 import os
 from pathlib import Path
+import utils.dataset_utils as ds_utils
 
 def prepare_logging(fid):
-    ds_utils.make_path("./log_files/")
-    log_fid = Path(fid).stem
-    logs = logging.getLogger(log_fid)
+    logs = logging.getLogger(__name__)
     logs.setLevel(logging.INFO)
     logs.propagate = False
+    log_fid = Path(fid).stem
 
     if not logs.handlers:
         # Logging info to log file
+        print("Logging output in ../log_files/")
         print("Logging output in ./log_files/")
         os.makedirs("./log_files/", exist_ok=True)
         file_path = ("./log_files/%s.log" % log_fid)
+        ds_utils.make_path("./log_files/")
         file = logging.FileHandler(file_path)
         fileformat = logging.Formatter("%(asctime)s:%(message)s")
         file.setLevel(logging.INFO)
         file.setFormatter(fileformat)
-
-    # Logging debug messages to stream
-    stream = logging.StreamHandler()
-    streamformat = logging.Formatter("[data_measurements_tool] %(message)s")
-    stream.setLevel(logging.DEBUG)
-    stream.setFormatter(streamformat)
-
-    logs.addHandler(file)
-    logs.addHandler(stream)
-
+        # Logging debug messages to stream
+        stream = logging.StreamHandler()
+        streamformat = logging.Formatter("[data_measurements_tool] %(message)s")
+        stream.setLevel(logging.DEBUG)
+        stream.setFormatter(streamformat)
+        logs.addHandler(file)
+        logs.addHandler(stream)
     return logs
