a37f847a5d0a1ab236d3bbfcc3cb787cfeafa14b
Ezi Ozoani <ozoaniezi@gmail.com>
2022-10-11 14:45:37 +0100

    no cache push

--- app.py
+++ app.py
@@ -18,24 +18,57 @@ import streamlit as st
 from os import mkdir
 from os.path import isdir
 from pathlib import Path
+
+
+import streamlit.components.v1 as components
+
+import streamlit as st
 from data_measurements.dataset_statistics import DatasetStatisticsCacheClass as dmt_cls
 import utils
 from utils import dataset_utils
 from utils import streamlit_utils as st_utils
 
+
+
+#"""
+#Examples:
+## When not in deployment mode
+#streamlit run app.py -- --live
+#
+## When deployed.
+#streamlit run app.py
+#"""
+
+from data_measurements import dataset_statistics
+from utils import dataset_utils
+from utils import streamlit_utils as st_utils
+
+
 logs = utils.prepare_logging(__file__)
 
-# Utility for sidebar description and selection of the dataset
-DATASET_NAME_TO_DICT = dataset_utils.get_dataset_info_dicts()
 
 
-# Set up the basic interface of the app.
+
+
 st.set_page_config(
     page_title="Demo to showcase dataset metrics",
     page_icon="https://huggingface.co/front/assets/huggingface_logo.svg",
     layout="wide",
     initial_sidebar_state="auto",
 )
+
+
+
+ 
+
+
+# Utility for sidebar description and selection of the dataset
+DATASET_NAME_TO_DICT = dataset_utils.get_dataset_info_dicts()
+
+
+
+
+
 
 def get_widgets(dstats):
     """
@@ -65,18 +98,32 @@ def get_widgets(dstats):
     return load_prepare_list, display_list
 
 def display_title(dstats):
-    title_str = f"### Showing: {dstats.dset_name} - {dstats.dset_config} - {dstats.split_name} - {'-'.join(dstats.text_field)}"
-    st.markdown(title_str)
+    #title_str = f"### Showing: {dstats.dset_name} - {dstats.dset_config} - {dstats.split_name} - {'-'.join(dstats.text_field)}"
+    #st.markdown(title_str)
     logs.info("showing header")
 
+
+
+def load_or_prepare_widgets(ds_args, show_embeddings, show_perplexities, live=False, use_cache=False):
+    """
+    Loader specifically for the widgets used in the app.
+    Args:
+        ds_args:
+        show_embeddings:
+        show_perplexities:
+        use_cache:
+     """
+
 def display_measurements(dataset_args, display_list, loaded_dstats,
-                         show_perplexities):
+                        show_perplexities):
     """Displays the measurement results in the UI"""
     if isdir(loaded_dstats.dataset_cache_dir):
-        show_column(loaded_dstats, display_list, show_perplexities)
+        show_column(loaded_dstats, display_list,show_perplexities)
     else:
         st.markdown("### Missing pre-computed data measures!")
         st.write(dataset_args)
+
+
 
 def display_initial_UI():
     """Displays the header in the UI"""
@@ -158,22 +205,46 @@ def show_column(dstats, display_list, show_perplexities, column_id=""):
     """
 
     # start showing stuff
-    st_utils.expander_header(dstats, DATASET_NAME_TO_DICT)
-    for widget_tuple in display_list:
+
+
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Dataset Description", "General Text Statistics", "Label Distribution", "Text Lengths","Text Duplicates","nPMI","Zipfs Law Fit"])#,"Text Embedding Clustering"])
+    
+    with tab1:
+        logs.info("showing header")
+        st_utils.expander_header(dstats, DATASET_NAME_TO_DICT)
+  
+        
+    ##TO:DO @Ezi --> incorperate 
+    #st_utils.expander_header(dstats, DATASET_NAME_TO_DICT)
+    i =1
+    for (widget_tuple, tab) in zip(display_list,[tab2, tab3, tab4, tab5, tab6, tab7]) :
         widget_type = widget_tuple[0]
         widget_fn = widget_tuple[1]
-        logs.info("showing %s." % widget_type)
-        try:
-            widget_fn(dstats, column_id)
-        except Exception as e:
-            logs.warning("Jk jk jk. There was an issue with %s:" % widget_type)
-            logs.exception(e)
+        with tab:
+            #tab_no = tabs
+            logs.info("showing %s" % widget_type)
+            try:
+                widget_fn(dstats, column_id)
+            except Exception as e:
+                logs.warning("Jk jk jk. There was an issue with %s:" % widget_type)
+                logs.exception(e)
+        i=i+1
+
+             
     # TODO: Fix how this is a weird outlier.
     if show_perplexities:
         st_utils.expander_text_perplexities(dstats, column_id)
     logs.info("Have finished displaying the widgets.")
+    # TODO: Fix how this is a weird outlier.
+
+  
+
 
 def main():
+    with open("style.css") as f:                                                # Reading style.css file and opening it 
+        st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html= True)       # Storing all styles in the streamlit markdown  &   unsafe_allow_html to true so that we can use html tags with the code  
+    #----read and run html file
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--live", default=False, required=False, action="store_true", help="Flag to specify that this is not running live.")
@@ -181,13 +252,57 @@ def main():
         "--pull_cache_from_hub", default=False, required=False, action="store_true", help="Flag to specify whether to look in the hub for measurements caches. If you are using this option, you must have HUB_CACHE_ORGANIZATION=<the organization you've set up on the hub to store your cache> and HF_TOKEN=<your hf token> on separate lines in a file named .env at the root of this repo.")
     arguments = parser.parse_args()
     live = arguments.live
+
+
+
+    # Sidebar description and selection
+
+    st.title("Data Measurements Tool")
+    st.markdown("""
+    This demo showcases the [dataset metrics as we develop them](https://huggingface.co/blog/data-measurements-tool).
+    
+    With this tool, you can:
+    
+    ✓ Dynamic loading of datasets in the lib
+
+    ✓ Fetching config and info without downloading the dataset
+
+    ✓ Propose the list of candidate text and label features to select
+
+    We are still working on:
+    
+    ✓ Implementing all the current tools
+    """,
+        unsafe_allow_html=True,)
+    # Get the sidebar details
+    st_utils.sidebar_header()
+    # Set up naming, configs, and cache path.
+    #compare_mode = st.sidebar.checkbox("Comparison mode")
+
+    # When not doing new development, use the cache.
+    use_cache = True
+
+   
+  
+    logs.warning("Using Single Dataset Mode")
+    
+    
+    st.sidebar.write('\n')
+    st.sidebar.write('\n')
+    st.sidebar.write('\n')
+   
+
     pull_cache_from_hub = arguments.pull_cache_from_hub
+
+    #dataset_args = st_utils.sidebar_selection(ds_name_to_dict)
 
     # Initialize the interface and grab the UI-provided arguments
     dataset_args = display_initial_UI()
 
     # TODO: Make this less of a weird outlier.
-    show_perplexities = st.sidebar.checkbox("Show text perplexities")
+    #show_perplexities = st.sidebar.checkbox("Show text perplexities")
+    show_perplexities = False
+
 
     # Initialize the main DMT class with the UI-provided arguments
     # When using the app (this file), try to use cache by default.
@@ -204,8 +319,8 @@ def main():
     # either because it was there already,
     # or we computed them (which we do when not live).
     # Write out on the UI what we have.
-    display_measurements(dataset_args, display_list, loaded_dstats,
-                         show_perplexities)
+    display_measurements(dataset_args, display_list, loaded_dstats,show_perplexities)
+
 
 if __name__ == "__main__":
     main()
